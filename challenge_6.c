c793604935fe02e8a9682c9ba3d1b1728a807ab3
Yaakoub tair <yaakoubt05@gmail.com>
2025-09-03 12:12:16 +0200

    challenge 6

---
+++ challenge_6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+int i=0,n,som=2;
+printf("sisir un nombre entier:\n");
+    scanf("%d",&n);
+    for ( i = 0; i <= n; i++)
+    {
+
+         printf("%d, ",som);
+         som=som+2;
+    }
+   
+
+
+
+
+    return 0;
+}
